b53c1e73dcecf77262f085ccb14981cd2bd3288d
Amit Singh <cm8204@hotmail.com>
2019-10-26 13:39:19 +0530

    Added counter time

--- app/src/main/java/com/example/countdowntimer/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.countdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView lTextField = findViewById(R.id.counter_time);
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                lTextField.setText(getString(R.string.count_time) + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                lTextField.setText(R.string.done);
+            }
+        }.start();
     }
 }
